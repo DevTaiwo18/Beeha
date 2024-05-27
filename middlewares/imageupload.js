210395cf7f9d7c924d2087e13dc5cc760116b388
DevTaiwo18 <adeyemitaiwo24434@gmail.com>
2024-05-27 11:24:47 +0100

    new update

--- middlewares/imageupload.js
+++ middlewares/imageupload.js
@@ -22,7 +22,7 @@ const processImages = async (req, res, next) => {
     try {
         const imagesPromises = req.files.map(file =>
             sharp(file.buffer)
-                .resize(900, 900) 
+                .resize(900, 700) 
                 .toFormat('jpeg') 
                 .jpeg({ quality: 90 }) 
                 .toBuffer() 
